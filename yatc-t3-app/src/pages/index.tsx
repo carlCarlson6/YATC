420f7806f48ff07c2aca66152883f754c3aefb5c
carl <carlos.acitores@colba.es>
2023-11-20 14:42:12 +0100

    developed by section

--- yatc-t3-app/src/pages/index.tsx
+++ yatc-t3-app/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { DiscordLogoIcon } from "@radix-ui/react-icons";
-import { Box, Button, Container, Flex, Heading, IconButton } from "@radix-ui/themes";
+import { CodeIcon, DiscordLogoIcon, GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Box, Button, Container, Flex, Heading, IconButton, Separator, Text } from "@radix-ui/themes";
 import { GetServerSideProps } from "next";
 import { getSession, signIn } from "next-auth/react";
 
@@ -24,9 +24,7 @@ export default () => (
 				<Button
 					variant={'outline'}
 					onClick={() => signIn()}
-					style={{
-						cursor: 'pointer'
-					}}
+					style={{ cursor: 'pointer' }}
 				>
 					<Flex
 						align={'center'}
@@ -38,6 +36,7 @@ export default () => (
 					</Flex>
 				</Button>
 			</Box>
+			<DevelopedBy />
 		</Flex>
 	</Container>
 )
@@ -56,3 +55,26 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 	return { props: { session } }
 }
+
+const DevelopedBy = () => (
+	<Box pt={'9'}>
+		<Text>Developed by Carlos A.D.</Text>
+		<Flex pt={'4'} align={'center'} justify={'center'} gap={'3'}>
+			<a href="https://github.com/carlCarlson6" target="_blank">
+				<IconButton variant={'outline'} style={{ cursor: 'pointer' }} >
+					<GitHubLogoIcon />
+				</IconButton>
+			</a>
+			<a href="https://github.com/carlCarlson6" target="_blank">
+				<IconButton variant={'outline'} style={{ cursor: 'pointer' }}>
+						<CodeIcon />
+				</IconButton>
+			</a>
+			<a href="https://www.linkedin.com/in/carlos-acitores-deval-a3914a1b/" target="_blank">
+				<IconButton variant={'outline'} style={{ cursor: 'pointer' }}>
+					<LinkedInLogoIcon />
+				</IconButton>
+			</a>
+		</Flex>
+	</Box>
+);
