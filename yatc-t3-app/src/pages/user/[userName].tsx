b51027a109a8e18a85cc04849a4c5910f974b404
carl <carlos.acitores@colba.es>
2023-11-26 16:45:47 +0100

    refactor loading of user data

--- yatc-t3-app/src/pages/user/[userName].tsx
+++ yatc-t3-app/src/pages/user/[userName].tsx
@@ -1,15 +1,12 @@
 import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { authPageGuard } from "yact/server/infrastructure/nextauth/page-auth-guard";
 import type { Timeline } from "yact/server/timeline/build-timeline";
-import { getUserTweets } from "yact/server/timeline/get-user-tweets";
 import type { User } from "yact/server/user/user";
 import { sanitizeQueryParams } from "../../server/infrastructure/sanitize-query-params";
-import { findUserProfile } from "yact/server/user/get-user-profile";
 import { Box } from "@radix-ui/themes";
 import { UserProfileDisplay } from "../../ui/user-profile/UserProfileDisplay";
 import { UserProfileControls } from "../../ui/user-profile/controls/UserProfileControls";
-import { checkIfFollowing } from "yact/server/user/follow/checkIfFollowing";
-import { countFollowers, countFollowing } from "yact/server/user/follow/counting";
+import { loadUserProfileData } from "../../server/user/loadUserProfileData";
 
 const UserProfile = ({user, tweets}: UserProfileProps) => (
   <Box>
@@ -39,22 +36,7 @@ export const getServerSideProps: GetServerSideProps<{
   },
 }> = async (context) => {
   const authResult = await authPageGuard(context);
-  if (authResult.result == "unauthenticated")
-    return authResult.redirectReturn;
-    
-  const sanitizedUserName = sanitizeQueryParams(context.query.userName);
-  const maybeUser = await findUserProfile(sanitizedUserName);
-  return !maybeUser
-    ? { notFound: true }
-    : { props: { 
-      user: { 
-        ...maybeUser, 
-        isOwnProfile: maybeUser.id === authResult.user.id, 
-        followed: await checkIfFollowing(authResult.user.id, maybeUser.id),
-        followersCount: await countFollowers(maybeUser.id),
-        followingCount: await countFollowing(maybeUser.id),
-      },
-      tweets: await getUserTweets(maybeUser.id),
-    }};
+  if (authResult.result == "unauthenticated") return authResult.redirectReturn;
+  const data = await loadUserProfileData(authResult.user, sanitizeQueryParams(context.query.userName));
+  return !data ? { notFound: true } : { props: { ...data } }
 }
-
